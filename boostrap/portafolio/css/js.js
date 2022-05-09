df2cc66496d4528944003e97bdc2deb1fecbb45f
Ivan Lopez <ivan.alm99@gmail.com>
2022-05-08 19:50:36 -0500

    cambios en desarrollo web

---
+++ boostrap/portafolio/css/js.js
@@ -0,0 +1,6 @@
+function caja(titulo){
+    titulo.style='color:rgba(233, 211, 10, 0.973);text-decoration-line: underline;';
+}
+function negro(titulo){
+    titulo.style='border:none;color:black'
+}
