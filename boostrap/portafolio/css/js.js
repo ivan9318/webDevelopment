dbe7a66be379b9292ac6b13b06df5020e346e5b9
Ivan Lopez <ivan.alm99@gmail.com>
2022-05-09 22:26:09 -0500

    Se realizaron cambios en el portafolio, cambio a  un formato flow y se agrego una libreria para estilos scroll

--- boostrap/portafolio/css/js.js
+++ boostrap/portafolio/css/js.js
@@ -1,6 +1,11 @@
+
 function caja(titulo){
     titulo.style='color:rgba(233, 211, 10, 0.973);text-decoration-line: underline;';
 }
 function negro(titulo){
     titulo.style='border:none;color:black'
+}
+
+function mostrar(){
+    document.getElementsByClassName('titulo').innerHTML='nuevo valor';
 }
